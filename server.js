2e981ee3d93a559279ee2a6d254c67f3bf3751c6
Matt Wigdahl <matt@leverpulley.com>
2020-02-05 21:05:21 -0700

    worked through bugs and cleaned up files.

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// Creating mongolab on ⬢ news-scaper... free
-// Welcome to mLab.  Your new subscription is being created and will be available shortly.  Please consult the mLab Add-on Admin UI to check on its progress.
-// Created mongolab-concentric-83491 as MONGODB_URI
-// Use heroku addons:docs mongolab to view documentation
-
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
@@ -31,7 +26,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/news-scraper1", { useNewUrlParser: true });
 
 
 app.get("/scrape", function(req, res) {
@@ -50,6 +45,8 @@ app.get("/scrape", function(req, res) {
                 console.log(err);  
             });
         });
+        res.send("Scrape Complete");
+        
     });
 });
 
@@ -61,58 +58,55 @@ app.get("/", function(req, res) {
       }
       res.render("index", hbsObj);
     }).catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   });
 
-// Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
     db.Article.find({})
       .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   });
 
-
-// Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
       .populate("note")
       .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   });
   
-  // Route for saving/updating an Article's associated Note
   app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
+    
     db.Note.create(req.body)
       .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        console.log("dbNote", dbNote);
         return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
       })
       .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+  app.get("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    console.log("req", req);
+    
+    db.Note.findByIdAndDelete(id)
+      .then(function(dbNote) {
+        res.json(dbNote)
+      })
+      .catch(function(err) {
         res.json(err);
       });
   });
